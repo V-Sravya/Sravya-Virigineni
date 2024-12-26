9cb2cad4608c90609403790ae8cb4a86c09146b0
Sravya Sri Virigineni <158924946+Sravya-Sri-V@users.noreply.github.com>
2024-12-25 20:06:04 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,3 @@
-const themeToggle = document.getElementById('theme-toggle');
-
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-
-  if (document.body.classList.contains('dark-theme')) {
-    themeToggle.textContent = 'Light Theme'; // Change button text
-    themeToggle.style.backgroundColor = '#ffffff'; // Change background color to white
-    themeToggle.style.color = '#000000'; // Change text color to black
-  } else {
-    themeToggle.textContent = 'Dark Theme'; // Revert button text
-    themeToggle.style.backgroundColor = '#2c2b2b'; // Revert background color
-    themeToggle.style.color = '#f0f0f0'; // Revert text color
-  }
-});
-
-
 // Select all elements to animate
 const animatedElements = document.querySelectorAll('.animate-up');
 
